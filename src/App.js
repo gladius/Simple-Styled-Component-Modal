c8f6ebccf404306dd176ac13b3642f00cbdc259e
gladius <gladius.thayalarajan@gmail.com>
2020-12-06 14:23:40 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Modal from "./components/modal";
-import { Button } from "./components/button";
+import { Button } from "./components/modal/modal.styles";
 
-const App =()=> {
-  const [active, setActive] = useState(false)
-     return (
-      <div>
-        <Button onClick={() =>setActive(true)}>Open Modal</Button>
-        <Modal
-          active={active}
-          hideModal={()=>setActive(false)}
-          title="Bla !! the title goes here"
-          footer={<Button>Footer Button</Button>}
-        >
-          Bla Bla Bla ..... the body goes here..
-        </Modal>
-      </div>
-    );
-}
+const App = () => {
+  const [active, setActive] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setActive(true)}>Open Modal</Button>
+      <Modal
+        active={active}
+        hideModal={() => setActive(false)}
+        title="Modal title goes here"
+        footer={<Button>Footer Button</Button>}
+      >
+        Modal body content goes here..
+      </Modal>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/modal/modal.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+import {
+  ModalBlock,
+  ModalBody,
+  ModalClose,
+  ModalContainer,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalTitle,
+} from "./modal.styles";
+
+const Modal = ({ title, footer, children, active, hideModal }) => {
+  return (
+    <Fragment>
+      {active && (
+        <ModalBlock>
+          <ModalOverlay onClick={() => hideModal()}></ModalOverlay>
+          <ModalContainer>
+            <ModalHeader>
+              <ModalTitle>{title}</ModalTitle>
+              <ModalClose onClick={() => hideModal()}>X</ModalClose>
+            </ModalHeader>
+            <ModalBody>{children}</ModalBody>
+            <ModalFooter>{footer}</ModalFooter>
+          </ModalContainer>
+        </ModalBlock>
+      )}
+    </Fragment>
+  );
+};
+export default Modal;
